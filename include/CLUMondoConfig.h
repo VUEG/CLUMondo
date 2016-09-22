37092eee7936155e0cac44129a72da7c1d1e06b7
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-09-22 13:45:51 +0200

    Add config header

---
+++ include/CLUMondoConfig.h
@@ -0,0 +1,9 @@
+#ifndef CLUMONDOCONFIG_H
+#define CLUMONDOCONFIG_H
+
+// the configured options and settings for Tutorial
+#define CLUMondo_VERSION_MAJOR 1
+#define CLUMondo_VERSION_MINOR 1
+#define CLUMondo_VERSION_PATCH 0
+
+#endif
