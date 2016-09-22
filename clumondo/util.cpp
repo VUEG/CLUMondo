a074ef277b61f46fa6b8dc1a8479589864db6946
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-09-22 16:59:25 +0200

    Add GPL3 header to all sources

--- clumondo/util.cpp
+++ clumondo/util.cpp
@@ -1,3 +1,29 @@
+/*****************************************************************
+ * This file is part of CLUMondo.
+ *
+ * CLUMondo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * CLUMondois distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CLUMondo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * CLUMondo utilities
+ * CLUMondo S.1.1
+ *
+ * Development:
+ *	Peter Verburg
+ *	31.3.2015
+ *
+ * All rights: Peter Verburg <peter.verburg@vu.nl>
+ *
+ ********************************************************************/
 #include "../include/util.h"
 
 #include <iostream>
--- include/globals.h
+++ include/globals.h
@@ -1,13 +1,24 @@
 /*****************************************************************
- * CLUMondo v S.1.1
+ * This file is part of CLUMondo.
+ *
+ * CLUMondo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * CLUMondois distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CLUMondo.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Development:
- *     Peter Verburg
- * The CLUE group
- * 8 feb 2015
+ *	Peter Verburg
+ *	31.3.2015
  *
- * All rights:
- * peter verburg
+ * All rights: Peter Verburg <peter.verburg@vu.nl>
  *
  ********************************************************************/
 
